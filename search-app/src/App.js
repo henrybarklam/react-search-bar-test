333ca168a14228afa67bd72ac33c42b89d8cc095
Henry Barklam <henry@optoinvest.com>
2023-12-19 08:51:40 -0500

    This doesn't work with most recent react

---
+++ search-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import SearchBar from './components/searchBar';
+
+function App() {
+  return (
+     <div classname='App'>
+       <SearchBar/>
+     </div>
+)}
+
+export default App;
