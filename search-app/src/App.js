2bacfdce3d66b449e330e3b19ac69710d7b1733c
Henry Barklam <henry@optoinvest.com>
2023-12-19 10:34:02 -0500

    Finally working after a few recompiles, flaky npm

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,13 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import SearchBar from './components/searchBar';
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./Components/List";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
+
   return (
-     <div classname='App'>
-       <SearchBar/>
-     </div>
-)}
+    <div className="main">
+      <h1>React Search</h1>
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          onChange={inputHandler}
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+      </div>
+      <List input={inputText} />
+    </div>
+  );
+}
 
 export default App;
